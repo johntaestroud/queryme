a2d06ee2b33b897c57b3fadb4a2f4dbb7f315fd2
unknown <johntae.stroud@gmail.com>
2018-07-13 20:29:08 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+ <head>
+   <title>Queryme | Search engine</title>
+ </head>
+ <body>
+
+    <div class="mainSection">
+
+        <div class="logoContainer">
+            <img src="" alt="">
+        </div>
+
+        <div class="searchContainer">
+
+            <form action="search.php" method="GET">  <!--page you're sending the data to | GET retrieve the value from the URL -->
+                <input class="searchBox" type="text" name="term">
+                <input class="searchButton" type="submit" value="Search">
+            </form>
+        </div>
+    
+    </div>
+    
+ </body>
+</html>
