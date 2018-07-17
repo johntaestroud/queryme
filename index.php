5a52a95a207770bcecfdba2e1076179fd1838c8c
unknown <johntae.stroud@gmail.com>
2018-07-17 15:49:17 -0400

    Styling the index page

--- index.php
+++ index.php
@@ -3,23 +3,29 @@
 <html>
  <head>
    <title>Queryme | Search engine</title>
+
+   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
  </head>
  <body>
 
-    <div class="mainSection">
+   <div class="wrapper indexPage">
 
-        <div class="logoContainer">
-            <img src="" alt="">
+        <div class="mainSection">
+
+            <div class="logoContainer">
+                <img src="" alt="">
+            </div>
+
+            <div class="searchContainer">
+
+                <form action="search.php" method="GET">  <!--page you're sending the data to | GET retrieve the value from the URL -->
+                    <input class="searchBox" type="text" name="term">
+                    <input class="searchButton" type="submit" value="Search">
+                </form>
+            </div>
+        
         </div>
-
-        <div class="searchContainer">
-
-            <form action="search.php" method="GET">  <!--page you're sending the data to | GET retrieve the value from the URL -->
-                <input class="searchBox" type="text" name="term">
-                <input class="searchButton" type="submit" value="Search">
-            </form>
-        </div>
-    
+        
     </div>
     
  </body>
