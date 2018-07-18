20ca1458b1e2a56feb74982a9572e0e6a52ae0de
unknown <johntae.stroud@gmail.com>
2018-07-17 22:44:27 -0400

    Implemented logo and searchbar

--- index.php
+++ index.php
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
-
 <html>
- <head>
-   <title>Queryme | Search engine</title>
+<head>
+	<title>Query Me | Search engine</title>
 
-   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
- </head>
- <body>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
-   <div class="wrapper indexPage">
+</head>
+<body>
 
-        <div class="mainSection">
+	<div class="wrapper indexPage">
+	
+		<div class="mainSection">
 
-            <div class="logoContainer">
-                <img src="" alt="">
-            </div>
+			<div class="logoContainer">
+				<img src="assets/images/qme.png">
+			</div>
 
-            <div class="searchContainer">
+			<div class="searchContainer">
 
-                <form action="search.php" method="GET">  <!--page you're sending the data to | GET retrieve the value from the URL -->
-                    <input class="searchBox" type="text" name="term">
-                    <input class="searchButton" type="submit" value="Search">
-                </form>
-            </div>
-        
-        </div>
-        
-    </div>
-    
- </body>
+				<form action="search.php" method="GET">
+
+					<input class="searchBox" type="text" name="term">
+					<input class="searchButton" type="submit" value="Search">
+
+				</form>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
 </html>
