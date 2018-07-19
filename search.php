d44e05976bf1827b2bd159150ef01356c5155f9d
unknown <johntae.stroud@gmail.com>
2018-07-19 14:25:00 -0400

    Implemented search tabs

--- search.php
+++ search.php
@@ -1,3 +1,16 @@
+<?php
+
+    if(isset($_GET["term"])) {
+        $term = $_GET["term"]; /*gets the term value from the URL*/
+    }
+    else {
+        exit("you must enter a search a term");
+    }
+
+    $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+   
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -27,8 +40,8 @@
                         <div class="searchBarContainer">
 
                             <input class="searchBox" type="text" name="term">
-                            <button>
-                                Search
+                            <button class="searchButton">
+                                <img src="assets/images/icons/search.png">
                             </button>
 
 
@@ -39,6 +52,25 @@
                 </div>
             
             </div>
+
+           <div class="tabsContainer">
+
+             <ul class="tabList">
+                <li class="<?php echo $type == 'sites' ? 'active' : ''?>">
+                    <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+                        Sites
+                    </a>
+                </li>
+
+                 <li class="<?php echo $type == 'images' ? 'active' : ''?>">
+                    <a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+                        Images
+                    </a>
+                </li>
+            
+             </ul>
+
+           </div>
 
         </div>
 
