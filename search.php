b6381379cd26cecfd668fff35cbdfa947357ee42
unknown <johntae.stroud@gmail.com>
2018-07-18 21:33:07 -0400

    Implemented style to the search bar section

--- search.php
+++ search.php
@@ -1,5 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>QueryMe | Search engine</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper">
+	
+        <div class="header">
+
+            <div class="headerContent">
+
+                <div class="logoContainer">
+                    <a href="index.php">
+                     <img src="assets/images/qme.png">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+
+                    <form action="search.php" method="GET">
+
+                        <div class="searchBarContainer">
+
+                            <input class="searchBox" type="text" name="term">
+                            <button>
+                                Search
+                            </button>
 
 
+                        </div>
 
-?>
+                    </form>
+
+                </div>
+            
+            </div>
+
+        </div>
+
+	</div>
+
+</body>
+</html>
